fead6cf059b92f217105317104cc552f4effcc70
Sinkyo <yhko1988@gmail.com>
2022-01-04 14:11:19 +0900

    :lipstick::list styles done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,60 @@
 class App {
   constructor() {}
 }
+
+/* 
+ <div class="clock">
+  <h1>00:00</h1>
+ </div>
+
+  <form action="" class="form">
+    <input type="text" class="form__input" placeholder="Enter your name" />
+  </form>
+
+  <h4 class="name">Yeong</h4>
+
+  <form action="" class="todo-form">
+    <input
+      type="text"
+      class="todo-form__input"
+      placeholder="Enter your tasks"
+    />
+  </form>
+
+  <div class="progress">
+    <div class="progress_count">2</div>
+    <progress id="progress-bar" value="2" max="10"></progress>
+  </div>
+
+  <section class="list">
+  
+    <div>
+      <h3>PENDING</h3>
+      <ul id="pending-list">
+        <li id="1640929192624" class="ui-sortable-handle">
+          <span>list 구현</span>
+          <div class="buttons">
+            <i class="far fa-edit"></i>
+            <i class="far fa-check-square"></i>
+            <i class="far fa-trash-alt"></i>
+          </div>
+        </li>
+      </ul>
+    </div>
+    
+    <div>
+      <h3>FINISHED</h3>
+      <ul id="finished-list">
+        <li id="1640929192624" class="ui-sortable-handle">
+          <span>weather 구현</span>
+          <div class="buttons">
+            <i class="far fa-edit"></i>
+            <i class="fas fa-backward"></i>
+            <i class="far fa-trash-alt"></i>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </section>
+*/
